9045141cde1b1e1bafc1e16777eacf3f94aaf2ef
sidharthsnairr <sidharthmidas07@gmail.com>
2023-05-12 00:39:54 +0530

    python triangle
    for looping,,,,,

---
+++ exam1.py
@@ -0,0 +1,19 @@
+# a = int(input("enter the number to print:"  ))
+# for i in range( a+1):
+#     for j in range ( i+1):
+#         print (a, end="  ")
+#     print()
+# a = int(input("enter the number to print:"  ))
+
+
+# for i in range( 1,6):
+#     for j in range ( i):
+#         print (i, end="  ")
+#     print()
+
+a = int(input("enter the number to print:"  ))
+for i in range( a+1):
+    for j in range (1,i+1 ):
+        print (j, end="  ")
+    print()
+
